9c935bd670fe3f9792644ff77cec4016999dbf4d
Cory Chaplin <cory.o.chaplin@gmail.com>
2023-01-22 11:32:28 -0600

    finished part 3

--- api/tags.js
+++ api/tags.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const tagsRouter = express.Router();
-const { getAllTags } = require('../db/index');
+const { getAllTags, getPostsByTagName } = require('../db/index');
 
 
 
@@ -19,6 +19,25 @@ tagsRouter.get('/', async (req, res) => {
     res.send({
         tags
     });
+});
+
+
+tagsRouter.get('/:tagName/posts', async (req, res, next) => {
+  const { tagName } = req.params;
+
+  try {
+    const matchingTags = await getPostsByTagName(tagName);
+    z
+    const matchingUserTags = matchingTags.filter(post => {
+      return post.active || (req.user && post.author.id === req.user.id);
+    });
+
+
+    res.send({matchingUserTags});
+
+  } catch ({ name, message }) {
+      next({ name, message });
+  }
 });
 
 module.exports = tagsRouter;
